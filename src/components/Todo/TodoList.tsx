62d0527151f2c10181a8d7e9afc019c7c7ece57a
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2023-01-25 13:41:48 +0700

    - React Table and MUI Integration

--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import MaterialReactTable, { MRT_ColumnDef } from "material-react-table";
+import React, { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Todo } from "../../models/todo";
 import store from "../../redux/store";
@@ -12,6 +13,10 @@ interface stateType {
 type AppDispatch = typeof store.dispatch;
 
 const TodoList: React.FC = () => {
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 2,
+  });
   const dispatch = useDispatch<AppDispatch>();
   const todos = useSelector((state: stateType) => state.todos);
 
@@ -19,12 +24,45 @@ const TodoList: React.FC = () => {
     dispatch(getTodosAsync());
   }, [dispatch]);
 
+  const columns = useMemo<MRT_ColumnDef<Todo>[]>(
+    () => [
+      {
+        header: "ID",
+        accessorKey: "id",
+      },
+      {
+        header: "Todo",
+        accessorKey: "todo",
+      },
+      {
+        header: "Completion",
+        accessorKey: "isDone",
+      },
+    ],
+    []
+  );
+
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <TodoBar todo={todo} key={todo.id} todos={todos} />
-      ))}
-    </div>
+    <>
+      <div className="todos">
+        {todos.map((todo) => (
+          <TodoBar todo={todo} key={todo.id} todos={todos} />
+        ))}
+      </div>
+      <MaterialReactTable
+        columns={columns}
+        data={todos}
+        muiTableBodyRowProps={{ hover: false }}
+        onPaginationChange={setPagination}
+        state={{ pagination }}
+        // enableColumnActions={false}
+        // enableColumnFilters={false}
+        // enablePagination={false}
+        // enableSorting={false}
+        // enableBottomToolbar={false}
+        // enableTopToolbar={false}
+      />
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,39 @@ import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import App from "./pages/Todo/App";
+import createTheme from "@mui/material/styles/createTheme";
+import { blue, grey, purple } from "@mui/material/colors";
+import { ThemeProvider } from "@emotion/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0ab0e8",
+    },
+    secondary: {
+      main: "#a0d4a3",
+    },
+    background: {
+      default: "#565454",
+      paper: "#4e4c4c",
+    },
+    text: {
+      primary: "rgba(255,255,255,0.7)",
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
